eaf604efab82f2c983e8649501ff7bb4d138f286
”Kawakami-Takeshi” <take.kawakami12@gmail.com>
2020-07-24 14:04:04 +0000

    practice21

---
+++ edit.blade.php
@@ -0,0 +1,17 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'エディット')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>エディット画面_課題3</h2>
+            </div>
+        </div>
+    </div>
+@endsection
